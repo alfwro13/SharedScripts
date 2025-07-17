1cbed8293f45942fdd39b668d6dfcf590242c6a6
alfwro13 <10945277+alfwro13@users.noreply.github.com>
2025-07-17 16:27:57 +0100

    More code refactor, added error handling for checking if external script exists, changed how groups are enumerated and displayed

--- Get-AduserInfo.ps1
+++ Get-AduserInfo.ps1
@@ -14,13 +14,21 @@ try {
 
 
 $details = get-aduser $UserName -Properties * 
-$details | select DisplayName,Description,EmailAddress,CanonicalName,whenCreated,LastBadPasswordAttempt,badPwdCount,PasswordLastSet,PasswordNeverExpires,PasswordExpired,LastLogonDate,LockedOut,HomeDirectory,HomeDrive,homeMDB,LogonCount,Manager,Modified,ObjectSID,AccountExpirationDate,Enabled
+$details | select DisplayName, Description, EmailAddress, CanonicalName, whenCreated, LastBadPasswordAttempt, badPwdCount, PasswordLastSet, PasswordNeverExpires, PasswordExpired, LastLogonDate, LockedOut, HomeDirectory, HomeDrive, homeMDB, LogonCount, Manager, Modified, ObjectSID, AccountExpirationDate, Enabled
 ""
 write-host "Used by VPN, SonicWall VPN, Meraki WiFi:"
 write-host "Allow Dial In (msNPAllowDialIn) option is set to: " $details.msNPallowDialIn -BackgroundColor Magenta -ForegroundColor Black
 ""
-.\Get-ADUserPasswordExpirationDate.ps1 $UserName
-write-host
-Write-host "This users is a member of the following security groups:" -BackgroundColor Yellow -ForegroundColor Black
-$details | select -ExpandProperty memberof | sort-object
-""
+
+if (Test-Path ".\Get-ADUserPasswordExpirationDate.ps1") {
+    .\Get-ADUserPasswordExpirationDate.ps1 $UserName
+} else {
+    Write-Warning "Password expiration script not found."
+}
+
+Write-Host ""
+Write-Host "This user is a member of the following security groups:" -BackgroundColor Yellow -ForegroundColor Black
+
+$details.memberof | ForEach-Object {
+    (Get-ADGroup $_).Name
+} | Sort-Object
